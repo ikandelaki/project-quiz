cd3d601f3981b6514f2e9b5f5aae7c538401761b
kandelseik <iraklikandelaki2@gmail.com>
2022-11-13 22:44:14 +0400

    Update headline

--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -3,9 +3,10 @@ import "../styles/StartMenu.css";
 const StartMenu = ({ start, setStart }) => {
   return (
     <div className={`start-menu menu-container ${start ? "disabled" : ""}`}>
-      <h1 className='start-heading'>
+      <h1 className='start-heading'>INSURANCE</h1>
+      <h2 className='start-heading-description'>
         Take the test and insure the risk of getting a low grade!
-      </h1>
+      </h2>
       <div className='btn start-btn' onClick={() => setStart(true)}>
         Start
       </div>
